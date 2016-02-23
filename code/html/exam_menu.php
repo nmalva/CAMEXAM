e114c5e23c5fef961727c3cf4008a1fffd573f55
Nico Malva <nmalva@hotmail.com>
2016-02-22 22:06:20 -0300

    Punto

--- code/html/exam_menu.php
+++ code/html/exam_menu.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <!--<![endif]-->
-<!-- BEGIN HEADD -->
+<!-- BEGIN HEADDD -->
 <head>
 <meta name="tipo_contenido"  content="text/html;" http-equiv="content-type" charset="utf-8">
 <!--  BEGIN INCLUDE CLASSES -->
