dc5cd4244517bca6e9845c06c038b888bc5e8a41
Nico Malva <nmalva@hotmail.com>
2016-02-22 22:05:12 -0300

    Fogueo

--- code/html/exam_menu.php
+++ code/html/exam_menu.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <!--<![endif]-->
-<!-- BEGIN HEAD -->
+<!-- BEGIN HEADD -->
 <head>
 <meta name="tipo_contenido"  content="text/html;" http-equiv="content-type" charset="utf-8">
 <!--  BEGIN INCLUDE CLASSES -->
